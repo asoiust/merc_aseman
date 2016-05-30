bf88730b1a86c450095de2debc1ecd14a3d244ad
sargdsra <sargdsra@gmail.com>
2016-05-30 14:49:29 +0430

    session added

--- flaskapp.py
+++ flaskapp.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'sargdsra'
 
-from flask import Flask, render_template, session, request, json
+from flask import Flask, render_template, session, request, json, redirect, url_for
 from database import check_user, add_user, search, get_summary, get_post
 
 app = Flask(__name__)
@@ -46,78 +46,83 @@ def f_sign_up():
 
 @app.route('/search')
 def f_search():
-    search_dict = {}
-    """static_possible_search_args = ["word", "overall", "genre"]
-        possible_search_args = ["min_storage", "max_storage", "min_price", "max_price"]
-        possible_search_args += ["min_discount", "max_discount", "min_statics", "max_statics", "min_release_date"]
-        possible_search_args += ["max_release_date", "min_os", "max_os", "min_processor", "max_processor"]
-        possible_search_args += ["min_memory", "max_memory", "min_graphics", "max_graphics", "min_directX"]
-        possible_search_args += ["max_directX", "min_reviews", "max_reviews"]"""
-    search_dict["min_os"] = request.args.get("os", "", type=str)
-    search_dict["min_graphics"] = request.args.get("graphic_card", "", type=str)
-    search_dict["min_processor"] = request.args.get("proccessor", "", type=str)
-    search_dict["min_directX"] = ""
-    search_dict["word"] = ""
-    search_dict["overall"] = ""
-    search_dict["genre"] = ""
-    search_dict["min_storage"] = ""
-    search_dict["max_storage"] = ""
-    search_dict["min_price"] = ""
-    search_dict["max_price"] = ""
-    search_dict["min_discount"] = ""
-    search_dict["max_discount"] = ""
-    search_dict["min_statics"] = ""
-    search_dict["max_statics"] = ""
-    search_dict["min_release_date"] = ""
-    search_dict["max_release_date"] = ""
-    search_dict["max_os"] = ""
-    search_dict["max_processor"] = ""
-    search_dict["min_memory"] = ""
-    search_dict["max_memory"] = ""
-    search_dict["max_graphics"] = ""
-    search_dict["max_directX"] = ""
-    search_dict["min_reviews"] = "1100"
-    search_dict["max_reviews"] = ""
-    print "123"
-    if search(search_dict):
-        stup = list(search(search_dict))
-        slis = [list(i) for i in stup]
-        for item in slis:
-            item[2] = str(item[2])
-        return json.dumps(slis)
-    return "0"
-
+    if session.get("user"):
+	    search_dict = {}
+	    """static_possible_search_args = ["word", "overall", "genre"]
+		possible_search_args = ["min_storage", "max_storage", "min_price", "max_price"]
+		possible_search_args += ["min_discount", "max_discount", "min_statics", "max_statics", "min_release_date"]
+		possible_search_args += ["max_release_date", "min_os", "max_os", "min_processor", "max_processor"]
+		possible_search_args += ["min_memory", "max_memory", "min_graphics", "max_graphics", "min_directX"]
+		possible_search_args += ["max_directX", "min_reviews", "max_reviews"]"""
+	    search_dict["min_os"] = request.args.get("os", "", type=str)
+	    search_dict["min_graphics"] = request.args.get("graphic_card", "", type=str)
+	    search_dict["min_processor"] = request.args.get("proccessor", "", type=str)
+	    search_dict["min_directX"] = ""
+	    search_dict["word"] = ""
+	    search_dict["overall"] = ""
+	    search_dict["genre"] = ""
+	    search_dict["min_storage"] = ""
+	    search_dict["max_storage"] = ""
+	    search_dict["min_price"] = ""
+	    search_dict["max_price"] = ""
+	    search_dict["min_discount"] = ""
+	    search_dict["max_discount"] = ""
+	    search_dict["min_statics"] = ""
+	    search_dict["max_statics"] = ""
+	    search_dict["min_release_date"] = ""
+	    search_dict["max_release_date"] = ""
+	    search_dict["max_os"] = ""
+	    search_dict["max_processor"] = ""
+	    search_dict["min_memory"] = ""
+	    search_dict["max_memory"] = ""
+	    search_dict["max_graphics"] = ""
+	    search_dict["max_directX"] = ""
+	    search_dict["min_reviews"] = "1100"
+	    search_dict["max_reviews"] = ""
+	    print "123"
+	    if search(search_dict):
+		stup = list(search(search_dict))
+		slis = [list(i) for i in stup]
+		for item in slis:
+		    item[2] = str(item[2])
+		return json.dumps(slis)
+	    return "0"
+    return redirect(url_for("f_home"))
 
 @app.route('/summary')
 def f_summary():
-    page_number = request.args.get("page_number", "", type=str)
-    print "123"
-    if get_summary(page_number):
-        stup = list(get_summary(page_number))
-        slis = [list(i) for i in stup]
-        for item in slis:
-            item[2] = str(item[2])
-        return json.dumps(slis)
-    return "0"
+    if session.get("user"):
+        page_number = request.args.get("page_number", "", type=str)
+        print "123"
+        if get_summary(page_number):
+            stup = list(get_summary(page_number))
+            slis = [list(i) for i in stup]
+            for item in slis:
+                item[2] = str(item[2])
+            return json.dumps(slis)
+        return "0"
+    return redirect(url_for("f_home"))
 
 
 @app.route('/game')
 def f_game():
-    inf = request.args.get("inf", "", type=str)
-    print "123"
-    if get_post(inf):    
-        stup = list(get_post(inf))
-        stup[2] = str(stup[2])
-        return json.dumps(stup)
-    return "0"
+    if session.get("user"):
+        inf = request.args.get("inf", "", type=str)
+        print "123"
+        if get_post(inf):    
+            stup = list(get_post(inf))
+            stup[2] = str(stup[2])
+            return json.dumps(stup)
+        return "0"
+    return redirect(url_for("f_home"))
 
 
 
 @app.route("/p_search", methods=['POST'])
 def f_go_search():
-    if request.method == 'POST':
+    if request.method == 'POST' and session.get("user"):
         return render_template("ad-search.html")
-
+    return render_template("main.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=4958)
